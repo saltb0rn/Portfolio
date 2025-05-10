a05e6ab08ca1deada29830e6ab92e4844b4b4c00
saltb0rn <asche34@outlook.com>
2025-05-10 16:58:37 +0800

    finish creation page

--- src/views/MusicVisualizerView.vue
+++ src/views/MusicVisualizerView.vue
@@ -22,27 +22,26 @@ onMounted(() => {
 
       <div class="description">
 
-        <h3 class="title border-5">Gabor/Voronoi Mix Music Visualizer</h3>
+        <div class="title border-5">
+          <h3>Gabor/Voronoi Mix Music Visualizer</h3>          
+        </div>
 
-        <div>
+        <div class="workinfo border-1">
 
-          <div>项目来源: <a href="https://www.youtube.com/watch?v=QRpCmBZpBU0&ab_channel=Visionary3D">YouTube</a></div>
+          <p>项目来源: <a href="https://www.youtube.com/watch?v=QRpCmBZpBU0&ab_channel=Visionary3D">YouTube</a>, 结合 Gabor Noise 和 Voronoi Noise 两种噪声来实现视觉效果, 视频没有给出最终代码, 为此参照思路并做出了一些调整得到目前的效果.
+          </p>
 
-        <div>结合 Gabor Noise 和 Voronoi Noise 两种噪声来实现视觉效果.</div>
-
-        <div>视频没有给出最终代码, 为此参照思路并做出了一些调整得到目前的效果.</div>
-        
         </div>
 
       </div>
 
-      <div class="musicinfo">
-        <div>演示曲目: 街头霸王 6 的 Mr. Top Player (Jamie's Theme).</div>
+      <div class="musicinfo border-1">
+        <p>演示曲目: 街头霸王 6 的 Mr. Top Player (Jamie's Theme).</p>
         <img />
       </div>
 
-      <div class="instruction">
-        点击进行播放
+      <div class="instruction border-1">
+        <p>点击进行播放</p>
       </div>
     </div>
 
